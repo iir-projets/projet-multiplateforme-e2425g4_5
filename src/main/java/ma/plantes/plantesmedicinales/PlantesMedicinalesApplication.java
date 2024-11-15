4d68de03c6d78d88175c1be02c104ac12d14fad1
dohatir <dohagit0@gmail.com>
2024-11-15 21:02:27 +0100

    projet backend

---
+++ src/main/java/ma/plantes/plantesmedicinales/PlantesMedicinalesApplication.java
@@ -0,0 +1,13 @@
+package ma.plantes.plantesmedicinales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantesMedicinalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantesMedicinalesApplication.class, args);
+    }
+
+}
