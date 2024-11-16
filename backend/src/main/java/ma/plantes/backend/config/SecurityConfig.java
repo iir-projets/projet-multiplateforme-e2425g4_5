bd5b64e004c351691e0190a5e167c48d39370d06
Zaha Fatima zahra <fatimazahra.zaha1504@gmail.com>
2024-11-16 22:38:08 +0100

    user config

---
+++ backend/src/main/java/ma/plantes/backend/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package ma.plantes.backend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
