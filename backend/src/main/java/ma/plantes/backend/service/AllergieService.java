6b11bd6d154a389a47f8da265241b769295d7148
dohatir <dohagit0@gmail.com>
2024-11-30 00:04:23 +0100

    service : allergie/maladie/medicament

--- backend/src/main/java/ma/plantes/backend/service/AllergieService.java
+++ backend/src/main/java/ma/plantes/backend/service/AllergieService.java
@@ -1,7 +1,25 @@
 package ma.plantes.backend.service;
 
+import ma.plantes.backend.entities.Allergie;
+import ma.plantes.backend.repositories.AllergieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AllergieService {
+
+    @Autowired
+    private AllergieRepository allergieRepository;
+
+   public void ajouterAllergie(Allergie allergie){
+       allergieRepository.save(allergie);
+   }
+
+   public boolean supprimerAllergie(Long id){
+       if (allergieRepository.existsById(id)){
+           allergieRepository.deleteById(id);
+           return true;
+       }
+       return false;
+   }
 }
--- backend/src/main/java/ma/plantes/backend/service/MaladieService.java
+++ backend/src/main/java/ma/plantes/backend/service/MaladieService.java
@@ -1,7 +1,25 @@
 package ma.plantes.backend.service;
 
+import ma.plantes.backend.entities.Maladie;
+import ma.plantes.backend.repositories.MaladieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MaladieService {
+
+    @Autowired
+    private MaladieRepository maladieRepository;
+
+    public void ajouterMaladie(Maladie maladie){
+        maladieRepository.save(maladie);
+    }
+
+    public boolean supprimerMaladie(Long id){
+        if (maladieRepository.existsById(id)){
+            maladieRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
