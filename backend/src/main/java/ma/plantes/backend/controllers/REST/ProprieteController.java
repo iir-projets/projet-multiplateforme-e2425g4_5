f12bdb20776e66df5507d6d1dea8b7f4ffba9991
dohatir <dohagit0@gmail.com>
2024-12-10 22:38:46 +0100

    directories : admin/client/service_sante

--- backend/src/main/java/ma/plantes/backend/controllers/REST/ProprieteController.java
+++ backend/src/main/java/ma/plantes/backend/controllers/REST/ProprieteController.java
@@ -1,4 +1,4 @@
-package ma.plantes.backend.controllers;
+package ma.plantes.backend.controllers.REST;
 
 import ma.plantes.backend.entities.Propriete;
 import ma.plantes.backend.service.ProprieteService;
