3845d89c3e4ec62567ff88d83a6a087bba92469e
dohatir <dohagit0@gmail.com>
2024-12-11 14:41:05 +0100

    delete thymeleaf

---
+++ service_sante/src/app/app.routes.ts
@@ -0,0 +1,37 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './service_sante/dashboard/dashboard.component';
+import { AllergiesComponent } from './service_sante/allergies/allergies.component';
+import { MaladiesComponent } from './service_sante/maladies/maladies.component';
+import { MedicamentsComponent } from './service_sante/medicaments/medicaments.component';
+import { ProfileComponent } from './client/profil/profil.component';
+
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'login',
+        pathMatch:'full',
+    },
+    {   path: 'login', 
+        component: LoginComponent 
+    },
+    {   path: 'register', 
+        component: RegisterComponent 
+    },
+    {
+        path : 'profil',
+        component : ProfileComponent
+    },
+    {   path: 'dashboard_sante', 
+        component: DashboardComponent, 
+        children:[
+            { path: 'allergies', component: AllergiesComponent },
+            { path: 'maladies', component: MaladiesComponent },
+            { path: 'medicaments', component: MedicamentsComponent }
+        ]
+    }
+    
+
+
+];
