84b84079413e80cab7ee30d34d26688613cc4e26
dohatir <dohagit0@gmail.com>
2024-12-16 00:34:57 +0100

    pop-up to add herb

--- service_sante/src/app/admin/addmodal/addmodal.component.ts
+++ service_sante/src/app/admin/addmodal/addmodal.component.ts
@@ -1,17 +1,47 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-addmodal',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './addmodal.component.html',
   styleUrl: './addmodal.component.css'
 })
 export class AddmodalComponent {
+   // Propriétés pour les champs du formulaire
+   herb = {
+    nom: '',
+    description: '',
+    precaution: '',
+    interaction: '',
+    region: '',
+    propriete: '',
+    image: ''
+  };
+
   constructor(public dialogRef: MatDialogRef<AddmodalComponent>) {}
 
-  // Méthode pour fermer le modal
   close(): void {
     this.dialogRef.close();
+  }
+
+  submit(): void {
+    console.log('Données de la nouvelle herbe :', this.herb);
+    this.dialogRef.close(this.herb);
   }
 }
