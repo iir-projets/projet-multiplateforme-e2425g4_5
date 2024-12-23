4b144c06abf71eae0cbbd9b6039dc6727a7f3f7c
guelbaoui <guelbaouihajar19@gmail.com>
2024-12-23 15:54:37 +0100

    link front article and details articles

--- service_sante/src/app/client/articles/articles.component.ts
+++ service_sante/src/app/client/articles/articles.component.ts
@@ -1,76 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article, ArticleService } from '../../../services/article/article.service';
+import { FooterComponent } from '../footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { NavbarComponent } from "../navbar/navbar.component";
-import { FooterComponent } from "../footer/footer.component";
-
-export interface Article {
-  id: number;
-  title: string;
-  description: string;
-  image: string;
-  isBookmarked: boolean;
-}
 
 @Component({
   selector: 'app-articles',
-  standalone: true,
-  imports: [CommonModule, FormsModule, NavbarComponent, FooterComponent],
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.css']
+  styleUrls: ['./articles.component.css'],
+  imports: [FooterComponent, NavbarComponent, CommonModule, RouterModule, FormsModule],
 })
-export class ArticleComponent {
-  searchTerm: string = '';
-  
-  // Liste des articles
-  articles: Article[] = [
-    {
-      id: 1,
-      title: 'Chamomile',
-      description: 'this is an article, this is an article, this is an article ...',
-      image: 'assets/images/plante2.jpg',
-      isBookmarked: false
-    },
-    {
-      id: 2,
-      title: 'Rosemary',
-      description: 'this is an article, this is an article, this is an article ...',
-      image: 'assets/images/plante8.jpg',
-      isBookmarked: false
-    },
-    {
-      id: 3,
-      title: 'Chamomile',
-      description: 'this is an article, this is an article, this is an article ...',
-      image: 'assets/images/plante5.jpg',
-      isBookmarked: false
-    },
-    {
-      id: 4,
-      title: 'Rosemary',
-      description: 'this is an article, this is an article, this is an article ...',
-      image: 'assets/images/plante3.jpg',
-      isBookmarked: false
-    }
-  ];
-
-  // Liste des articles sauvegardés
+export class ArticleComponent implements OnInit {
+  articles: Article[] = [];
   savedArticles: Article[] = [];
+  searchTerm: string = '';
 
-  toggleBookmark(article: Article) {
-    article.isBookmarked = !article.isBookmarked;
-  }
+  constructor(private articleService: ArticleService) {}
 
-  // Filtrer les articles en fonction du terme de recherche
-  get filteredArticles() {
-    return this.articles.filter(article =>
-      article.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-      article.description.toLowerCase().includes(this.searchTerm.toLowerCase())
+  ngOnInit(): void {
+    this.articleService.getArticles().subscribe(
+      (data) => {
+        this.articles = data;
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des articles', error);
+      }
     );
   }
 
-  // Sauvegarder tous les articles dans savedArticles
-  saveAllArticles() {
-    this.savedArticles = [...this.articles];  // Ajoute tous les articles à la liste des articles sauvegardés
+  get filteredArticles() {
+    return this.articles.filter(article =>
+      article.titre.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      article.contenu.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
